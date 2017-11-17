ea7c4f011f2a1e70baec93b9d2d2bbf4ceb2e7e5
evquiachon <30821546+evquiachon@users.noreply.github.com>
2017-11-17 22:17:54 +0800

    added anime category

---
+++ anime.js
@@ -0,0 +1,140 @@
+db.questions.insertMany([
+  {
+    question: "What legendary Pokemon did Ash see on the first day of his Pokemon journey?",
+    type: "Multiple choice",
+    difficulty: "Easy",
+    category: "Anime",
+    answer: "Ho-oh",
+    choices: ["Pikachu", "Ho-oh", "Articuno", "Lugia"] 
+  },
+  {
+    question: "What is Naruto's favorite food?",
+    type: "Text answer",
+    difficulty: "Easy",
+    category: "Anime",
+    answer: "Ramen"
+  },
+  {
+    question: "The Devil's Fruit that gave Luffy his powers is called the Flame-Flame Fruit.",
+    type: "True or False",
+    difficulty: "Easy",
+    category: "Anime",
+    answer: "False"
+  },
+  {
+    question: "Conan Edogawa's true identity is Shinichi Kudo.",
+    type: "True or False",
+    difficulty: "Easy",
+    category: "Anime",
+    answer: "True"
+  },
+  {
+    question: "What is Agumon's second digivolution form?",
+    type: "Multiple choice",
+    difficulty: "Easy",
+    category: "Anime",
+    answer: "Greymon",
+    choices: ["Garurumon", "Greymon", "Angemon", "Rosemon"]
+  },
+  {
+    question: "In the anime Fate Stay Night, what is Berserker's true name?",
+    type: "Multiple choice",
+    difficulty: "Medium",
+    category: "Anime",
+    answer: "Heracles",
+    choices: ["Lancelot", "Spartacus", "Frankenstein", "Heracles"]
+  },
+  {
+    question: "In the anime Death Note, what is L's real name?",
+    type: "Text answer",
+    difficulty: "Medium",
+    category: "Anime",
+    answer: "L Lawliet"
+  },
+  {
+    question: "In the anime Danganronpa, the real mastermind behind the killing game is Enoshima Junko.",
+    type: "True or False",
+    difficulty: "Medium",
+    category: "Anime",
+    answer: "True"
+  },
+  {
+    question: "In the anime Fairy Tail, what is the name of Natsu's patron dragon?",
+    type: "Multiple choice",
+    difficulty: "Medium",
+    category: "Anime",
+    answer: "Igneel",
+    choices: ["Igneel", "Grandine", "Makarov", "Happy"]
+  },
+  {
+    question: "In the anime Kuroko's Basketball, what is the number on Taiga's Seirin jersey?",
+    type: "Number answer",
+    difficulty: "Medium",
+    category: "Anime",
+    answer: 10
+  },
+  {
+    question: "In the anime Love Live, the title of the song performed by µ's on Season 2 Episode 3 is Snow Halation.",
+    type: "True or False",
+    difficulty: "Hard",
+    category: "Anime",
+    answer: "False"
+  },{
+    question: "In the anime Your Lie in April, what day was Kousei's piano competition which was also the day Kaori passed away?",
+    type: "Multiple Choice",
+    difficulty: "Hard",
+    category: "Anime",
+    answer: "February 18",
+    choices: ["February 18", "April 1", "December 14", "June 8"]
+  },
+  {
+    question: "In the anime Sword Art Online, how many survivors remained when Kirito completed SAO?",
+    type: "Number answer",
+    difficulty: "Hard",
+    category: "Anime",
+    answer: 6147
+  },
+  {
+    question: "In the anime Food Wars, what was the dish made by Erina Nakiri for the Breakfast Buffet Challenge on Season 1 Episode 13?",
+    type: "Text answer",
+    difficulty: "Hard",
+    category: "Anime",
+    answer: "Eggs Benedict"
+  },
+  {
+    question: "In the anime Katekyo Hitman Reborn, who is founder of the Vongola Famiglia and was also known as Vongola Primo?",
+    type: "Text answer",
+    difficulty: "Hard",
+    category: "Anime",
+    answer: "Giotto"
+  },
+   {
+    question: "In Ben 10 Alien Force, which was the first alien that Ben was able to transform into?",
+    type: "Text Answer",
+    difficulty: "Easy",
+    category: "TV Series",
+    answer: "Swampfire"
+  },
+   {
+    question: "In which episode of Arrow Season 2 was Barry Allen's first appearance?",
+    type: "Number answer",
+    difficulty: "Medium",
+    category: "TV Series",
+    answer: 8
+  },
+   {
+    question: "In The Flash Season 3, what is the true identity of Savitar?",
+    type: "Multiple Choice",
+    difficulty: "Hard",
+    category: "TV Series",
+    answer: "Barry Allen",
+    choices: ["Eobard Thawne", "Hunter Zolomon", "Wally West", "Barry Allen"]
+  },
+   {
+    question: "In Adventure Time, what is the animal that can be seen somewhere in every episode?",
+    type: "Text Answer",
+    difficulty: "Medium",
+    category: "TV Series",
+    answer: "snail"
+  }
+])
